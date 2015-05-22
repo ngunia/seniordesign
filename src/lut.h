d16b2a1fe88225a6704e50582e6761735a64d392
Nick <nickgunia@gmail.com>
2015-05-22 15:00:20 -0400

    adding look up table for overdrive

---
+++ src/lut.h
@@ -0,0 +1,17 @@
+/*
+ * lut.h
+ *
+ *  Created on: May 14, 2015
+ *      Author: Nick
+ */
+
+#ifndef LUT_H_
+#define LUT_H_
+
+#include "misc.h"
+
+extern const uint16_t overdrive_lut[4096];
+
+
+
+#endif /* LUT_H_ */
